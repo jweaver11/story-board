e77b3f0c266f38a0cfa6ca629a20f89be84a8ab5
jweaver11 <joshuatweaver2000@gmail.com>
2026-08-11 18:48:47 -0500

    Notes now look much more like note cards than just empty textfields

--- src/models/widgets/note.py
+++ src/models/widgets/note.py
@@ -4,8 +4,10 @@ import flet as ft
 from models.views.story import Story
 from models.widget import Widget
 from models.app import app
-from styles.text_fields import TextField, UnderlinedTextField
+from styles.text_fields import TextField, UnderlinedTextField, NoLabelTextField
+from styles.menu_option_style import MenuOptionStyle
 import asyncio
+from styles.colors import colors
     
 
 class Note(Widget):
@@ -28,9 +30,9 @@ class Note(Widget):
                 'tag': "note", 
                 'color': app.settings.data.get('widget_defaults', {}).get('note', {}).get('color'),
 
-                # Note segment data. Stored as list so we can duplicate titles
-                'segment_data': [ 
-                    {"title": "", "content": ""},
+                # Note card data. Stored as list so we can duplicate labels
+                'card_data': [ 
+                    {"label": "", "value": "", 'color': 'onsurface'},
                 ]
             })
 
@@ -43,108 +45,186 @@ class Note(Widget):
 
         self.padding = ft.Padding.all(10)   # Set padding
 
-        # Column to hold our segments textfields
-        segments_column = ft.Column(
-            expand=True, horizontal_alignment=ft.CrossAxisAlignment.CENTER, 
-            controls=[], scroll="auto", alignment=ft.MainAxisAlignment.START
-        )
-
         
-        # Adds our new segment to data and our column
-        async def create_segment(e=None):
-            self.data['segment_data'].append({"title": self.new_segment_tf.value, "content": ""})
-            self.update_data(**{'segment_data': self.data['segment_data']})
-            segments_column.controls.append(new_segment_textfield(len(self.data['segment_data']) - 1, self.new_segment_tf.value, ""))
-            segments_column.update()
-            self.new_segment_tf.value = ""
-            self.new_segment_tf.update()
-            add_segment_button.visible = True
-            add_segment_button.update()
+        # Adds our new card to data and our column
+        async def create_card(e=None):
+            self.data['card_data'].append({"label": self.new_card_tf.value, "value": "", 'color': "onsurface"})
+            self.update_data(**{'card_data': self.data['card_data']})
+            card_row.controls.append(new_card(len(self.data['card_data']) - 1, self.data['card_data'][-1]))
+            card_row.update()
+            self.new_card_tf.value = ""
+            self.new_card_tf.update()
+            add_card_button.visible = True
+            add_card_button.update()
             await asyncio.sleep(0.02)
-            await segments_column.scroll_to(offset=-1, duration=200)
+            await card_row.parent.scroll_to(offset=-1, duration=200)
+            
 
-        # Deletes a segment from data and our column
-        async def delete_segment(e: ft.Event):
-            index = e.control.data
-            if len(self.data['segment_data']) > index:
-                del self.data['segment_data'][index]
-                self.update_data(**{'segment_data': self.data['segment_data']})
-                segments_column.controls.pop(index)
-                segments_column.update()
-
-                for i, ctrl in enumerate(segments_column.controls):
-                    ctrl.data = i
-                    ctrl.suffix_icon.data = i
+        # Saves label when text field is unfocused
+        def save_card_label(e: ft.Event[ft.TextField]):
+            index = e.control.parent.parent.parent.data
+            if len(self.data['card_data']) > index:
+                self.data['card_data'][index]['label'] = e.control.value
+                self.update_data(**{'card_data': self.data['card_data']})
+                    
 
         # Saves content when text field is unfocused
-        async def save_segment(e):
-            index = e.control.data
-            if len(self.data['segment_data']) > index:
-                self.data['segment_data'][index]['content'] = e.control.value
-                self.update_data(**{'segment_data': self.data['segment_data']})
+        def save_card_value(e: ft.Event[ft.TextField]):
+            index = e.control.parent.parent.parent.data
+            
+            if len(self.data['card_data']) > index:
+                self.data['card_data'][index]['value'] = e.control.value
+                self.update_data(**{'card_data': self.data['card_data']})
 
-        # Gives us a new textfield for each note segment
-        def new_segment_textfield(idx: int, key: str='', value: str='') -> TextField:
-            tf = TextField(
-                value, expand=True, capitalization=ft.TextCapitalization.SENTENCES, 
-                multiline=True, label=key, dense=True, 
-                on_blur=save_segment, 
-                data=idx, 
-                suffix_icon=ft.IconButton(
-                    ft.Icons.DELETE_OUTLINE, ft.Colors.ERROR,
-                    tooltip=f"Delete segment {key}",
-                    on_click=delete_segment,
-                    mouse_cursor="click", data=idx
+        def get_card_options(idx: int) -> list[ft.Control]:
+            ''' Pops open a column of the menu options for this tree view item'''
+
+            async def handle_delete(e: ft.Event[ft.Control]):
+                if len(self.data['card_data']) > idx:
+                    del self.data['card_data'][idx]
+                    self.update_data(**{'card_data': self.data['card_data']})
+                    card_row.controls.pop(idx)
+                    card_row.update()
+                    for i, ctrl in enumerate(card_row.controls):
+                        ctrl.data = i
+                    await self.story.close_menu()
+
+            async def handle_color_change(e: ft.Event[ft.Control]):
+                new_color = e.control.data
+                if len(self.data['card_data']) > idx:
+                    self.data['card_data'][idx]['color'] = new_color
+                    self.update_data(**{'card_data': self.data['card_data']})
+                    card_row.controls[idx].content.bgcolor = ft.Colors.with_opacity(0.05, new_color)
+                    card_row.controls[idx].content.update()
+                    await self.story.close_menu()
+
+
+            return [
+                MenuOptionStyle(
+                    ft.SubmenuButton(
+                        ft.Row([
+                            ft.Icon(ft.Icons.COLOR_LENS_OUTLINED, ft.Colors.PRIMARY), 
+                            ft.Text("Color", weight=ft.FontWeight.BOLD, expand=True),
+                            ft.Icon(ft.Icons.ARROW_RIGHT),
+                        ], expand=True),
+                        [ft.MenuItemButton(color.capitalize(), style=ft.ButtonStyle(color), on_click=handle_color_change, data=color) for color in colors],
+                        menu_style=ft.MenuStyle(alignment=ft.Alignment.TOP_RIGHT, padding=ft.Padding.all(0)),
+                        style=ft.ButtonStyle(padding=ft.Padding.only(left=8), shape=ft.RoundedRectangleBorder(radius=4), mouse_cursor="click"),
+                        tooltip="Change this widget's color"
+                    ),
+                    no_padding=True, no_effects=True
                 ),
+                MenuOptionStyle(
+                    on_click=handle_delete,
+                    content=ft.Row([
+                        ft.Icon(ft.Icons.DELETE_OUTLINE_OUTLINED, size=20, color=ft.Colors.ERROR),
+                        ft.Text("Delete Card", weight=ft.FontWeight.BOLD)
+                    ], alignment=ft.MainAxisAlignment.START, spacing=10),
+                )
+            ]
+
+        # Gives us a new textfield for each note card
+        def new_card(idx: int, data: dict={}) -> TextField:
+            label = data.get('label', '')
+            value = data.get('value', '')
+            color = data.get('color', 'onsurface')
+
+            #ft.IconButton(ft.Icons.DELETE_OUTLINE_OUTLINED, on_click=delete_card, tooltip="Delete this card"),
+            # Top textfield for the label
+            label_tf = ft.TextField(
+                value=label,
+                dense=True, multiline=True, width=400,
+                border_color=ft.Colors.TRANSPARENT,
+                capitalization=ft.TextCapitalization.SENTENCES,
+                text_style=ft.TextStyle(size=14, weight=ft.FontWeight.BOLD),
+                suffix_icon=ft.GestureDetector(
+                    ft.IconButton(
+                        ft.Icons.MORE_VERT, ft.Colors.ON_SURFACE_VARIANT, 
+                        on_click=lambda e: self.story.open_menu(get_card_options(e.control.parent.parent.parent.parent.parent.data)),
+                        mouse_cursor=ft.MouseCursor.CLICK,
+                    ),
+                    on_hover=lambda e: self.set_mouse_coords(e),
+                    hover_interval=30,
+                ),
+                on_blur=save_card_label
             )
-            tf.bgcolor = ft.Colors.SURFACE_CONTAINER_HIGHEST
-            return tf
 
-        # Go through the note data and load the segments
-        for idx, segment in enumerate(self.data.get('segment_data', [])):
-            key = segment.get('title', '')
-            value = segment.get('content', '')
-            segments_column.controls.append(new_segment_textfield(idx, key, value))
+            # Bottom textfield for the body of the card
+            body_tf = ft.TextField(
+                dense=True,
+                border_color=ft.Colors.TRANSPARENT,
+                text_style=ft.TextStyle(size=14),
+                multiline=True,
+                capitalization=ft.TextCapitalization.SENTENCES,
+                value=value, expand=True, on_blur=save_card_value,
+            )  
+            
+            
+            card = ft.Card(
+                ft.Container(
+                    ft.Column([
+                        label_tf,
+                        ft.Divider(2, 2, leading_indent=10, trailing_indent=10),
+                        body_tf
+                    ], spacing=0, expand=True),
+                    bgcolor=ft.Colors.with_opacity(0.05, color),
+                ),
+                bgcolor=ft.Colors.SURFACE_CONTAINER_HIGH,
+                height=300, width=400,
+                shape=ft.RoundedRectangleBorder(radius=4),
+                data=idx
+            )
+            return card
 
-        # Show the textfield to label the new segment
-        async def _create_new_segment_clicked(e):
-            add_segment_button.visible = False
-            add_segment_button.update()
-            self.new_segment_tf.value = ""
-            self.new_segment_tf.visible = True
-            self.new_segment_tf.label = "New Segment Label"
-            self.new_segment_tf.update() 
-            await self.new_segment_tf.focus()
+        # Column to hold our cards textfields
+        card_row = ft.Row(
+            controls=[], 
+            wrap=True, alignment=ft.MainAxisAlignment.START, expand=True,
+        )
+
+        # Go through the note data and load the cards
+        for idx, card_data in enumerate(self.data.get('card_data', [])):
+            card_row.controls.append(new_card(idx, card_data))
+
+        # Show the textfield to label the new card
+        async def _create_new_card_clicked(e):
+            add_card_button.visible = False
+            add_card_button.update()
+            self.new_card_tf.value = ""
+            self.new_card_tf.visible = True
+            self.new_card_tf.label = "New card Label"
+            self.new_card_tf.update() 
+            await self.new_card_tf.focus()
 
         # Hide the textfield
-        async def _hide_new_segment_tf(e):
-            self.new_segment_tf.visible = False
-            self.new_segment_tf.update()
-            add_segment_button.visible = True
-            add_segment_button.update()
+        async def _hide_new_card_tf(e):
+            self.new_card_tf.visible = False
+            self.new_card_tf.update()
+            add_card_button.visible = True
+            add_card_button.update()
 
-        # Button to click to add a new segment
-        add_segment_button = ft.Button(
-            "Add Segment", #ft.Icons.ADD_CIRCLE_OUTLINE_OUTLINED, ft.Colors.PRIMARY,
-            tooltip="Add a new segment to your note.", 
-            on_click=_create_new_segment_clicked, 
+        # Button to click to add a new card
+        add_card_button = ft.Button(
+            "Add Card", #ft.Icons.ADD_CIRCLE_OUTLINE_OUTLINED, ft.Colors.PRIMARY,
+            tooltip="Add a new card to your note.", 
+            on_click=_create_new_card_clicked, 
             style=ft.ButtonStyle(mouse_cursor=ft.MouseCursor.CLICK, text_style=ft.TextStyle(weight=ft.FontWeight.W_500, size=20)),
             bgcolor=ft.Colors.SURFACE_CONTAINER_LOWEST
         )
 
-        self.new_segment_tf = ft.TextField(
-            label="Add New Segment", dense=True, 
+        self.new_card_tf = ft.TextField(
+            label="Add New Card", dense=True, 
             capitalization=ft.TextCapitalization.WORDS,
-            on_blur=_hide_new_segment_tf,
-            on_submit=create_segment, visible=False, autofocus=True,
+            on_blur=_hide_new_card_tf,
+            on_submit=create_card, visible=False, autofocus=True,
             bgcolor=ft.Colors.SURFACE_CONTAINER_LOWEST
         ) 
 
 
         self.content = ft.Stack([
-            segments_column,
+            ft.Column([card_row], expand=True, alignment=ft.MainAxisAlignment.START, scroll=ft.ScrollMode.AUTO),
             ft.Column([
-                self.new_segment_tf,
-                add_segment_button, 
+                self.new_card_tf,
+                add_card_button, 
             ], alignment=ft.MainAxisAlignment.END, horizontal_alignment=ft.CrossAxisAlignment.END, expand=True,)
-        ], alignment=ft.Alignment.TOP_RIGHT, expand=True)
+        ], alignment=ft.Alignment.TOP_LEFT, expand=True)
